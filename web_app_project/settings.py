8da1af70b923e46c837c8f461dc67c3cc5912ba7
Fernanda Bisinoto <bisinoto@amazon.com>
2022-09-18 16:47:22 +0100

    Changing hosts

--- web_app_project/settings.py
+++ web_app_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%sbj&dryg9^czopaf-cb^7yf_oss-(ns+-3b+bhgr3dv@h=j#p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-app-project-assignment.herokuapp.com']
 
 
 # Application definition
