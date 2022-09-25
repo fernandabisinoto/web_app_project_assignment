126107cd76ac294677d388b4d4a3c4f57de0a834
Fernanda Bisinoto <bisinoto@amazon.com>
2022-09-25 13:57:56 +0100

    Updating settings

--- web_app_project/settings.py
+++ web_app_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import Config
 
 import os
 import dj_database_url
@@ -25,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = Config('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
