1b54eb07e23b7fdfdf8ec938fa5904a8adc79e7d
Fernanda Bisinoto <bisinoto@amazon.com>
2023-07-05 18:33:12 +0100

    Testing new branch

--- web_app_project/settings.py
+++ web_app_project/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+"""
+Adding comment for testing purposes 
+"""
+
 from pathlib import Path
 from decouple import config
 
